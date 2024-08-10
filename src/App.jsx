a77effdc156165c82788a64cb8a57a7f5c2f0cc2
orlandoroca <rocahuapaya@hotmail.com>
2024-08-09 22:50:06 -0500

    update:route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import AuthenticationPage from "./authentication/pages/AuthenticationPage/AuthenticationPage.jsx";
 import './App.css'
 import {BrowserRouter, Route, Routes, useNavigate} from "react-router-dom";
-import ProfilePage from "./dataUpdate/pages/ProfilePage.jsx";
 import {AuthenticationService} from "./authentication/services/Authentication.service.js";
 import {useEffect} from "react";
 import Application from "./share/pages/Application/application.jsx";
